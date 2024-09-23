9b2d9678f59b7b5a5e686b778227daf4da0fb4b9
leonardo <leonardodanieltellez05@gmail.com>
2024-09-23 14:41:39 -0600

    Integration of Laravel UI, AdminLTE, and roles/permissions

---
+++ database/seeders/UserSeed.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+
+class UserSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('12345678'),
+            'remember_token' => Str::random(10),
+
+        ])->assignRole('admin');
+        
+
+        User::create([
+
+            'name' => 'leo',
+            'email' => 'leo@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('12345678'),
+            'remember_token' => Str::random(10),
+
+        ])->assignRole('lender');
+    }
+}
